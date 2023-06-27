4b17ed1707e913b11eb6d9cd4ecebc98cb4dcdaf
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2023-06-27 14:20:10 +0300

    Add project files.

---
+++ NavPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Repro
+{
+    public partial class NavPage : ContentPage
+    {
+        public NavPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
